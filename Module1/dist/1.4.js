3801ffd9314cef587c9884cba2d5128352438d77
Chishty313 <chishty828rahman@gmail.com>
2024-05-02 12:29:18 +0600

    Done till Basic Data types

---
+++ Module1/dist/1.4.js
@@ -0,0 +1,18 @@
+"use strict";
+// Basic Data Types
+// String
+let firstName = "Chishty";
+// Number
+let roll = 123;
+// Boolean
+let isAdmin = true;
+// Undefined
+let x = undefined;
+// Null
+let y = null;
+let d;
+d = 123;
+// Array
+let friends = ["rachel", "moniaca"];
+friends.push(7);
+console.log(friends);
---
+++ Module1/src/1.4.ts
@@ -0,0 +1,39 @@
+// Basic Data Types
+
+// String
+
+let firstName: string = "Chishty";
+
+// Number
+
+let roll: number = 123;
+
+// Boolean
+
+let isAdmin: boolean = true;
+
+// Undefined
+
+let x: undefined = undefined;
+
+// Null
+
+let y: null = null;
+
+let d: number;
+
+d = 123;
+
+// Array
+
+let friends: string[] = ["rachel", "moniaca"];
+
+friends.push(7);
+
+// Tuple --> array --> order --> type of value
+
+let coordinates: [number, number] = [1, 5];
+
+let ageName: [number, string] = [50, "Mr. X"];
+
+ageName[0] = "Mr. Y";
