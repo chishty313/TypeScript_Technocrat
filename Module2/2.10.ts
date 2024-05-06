3ef77aa04ca828bae1dc7fea40f9b4be6f658b55
Chishty313 <chishty828rahman@gmail.com>
2024-05-06 15:06:41 +0600

    Started utility types

--- Module2/2.10.ts
+++ Module2/2.10.ts
@@ -11,13 +11,23 @@
         height: number;
         width: number;
     }
+
+    type Height = AreaNumber["height"] // Look up type
     // type AreaString = {
     //     height: string;
     //     width: string;
     // }
     // keyof AreaNumber => "height" | "width"
 
-    type AreaString = {
-        [ key in keyof AreaNumber ] : string
+    // T => {height: string, width: number}
+    // key => T["width"]
+
+    type AreaString<T> = {
+        [ key in keyof T ] : T[key];
+    }
+
+    const area1: AreaString<{height: string, width: number}> = {
+        height: "100",
+        width: 50
     }
 }
