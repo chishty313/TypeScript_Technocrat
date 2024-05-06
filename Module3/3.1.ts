c94ddb5d80e8583faaf6745385536e265138495a
Chishty313 <chishty828rahman@gmail.com>
2024-05-06 16:56:29 +0600

    Completed till Half of Class and Object. Till starting of the  method creation

---
+++ Module3/3.1.ts
@@ -0,0 +1,14 @@
+{
+    // OOP - class
+    class Animal {
+        name: string;
+        species: string;
+        sound: string;
+
+        constructor(name: string, species: string, sound: string){
+            this.name = name;
+            this.sound = sound;
+            this.species = species;
+        }
+    }
+}
