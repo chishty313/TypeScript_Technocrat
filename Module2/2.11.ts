876a98f948ed83032665dca7d020c59868438987
Chishty313 <chishty828rahman@gmail.com>
2024-05-06 15:30:51 +0600

    Completed till Utility Types

--- Module2/2.11.ts
+++ Module2/2.11.ts
@@ -1,5 +1,48 @@
 {
     // Utility types
+    // Pick
 
-    
+    type Person = {
+        name: string;
+        age: number;
+        email?: string;
+        contactNo: string
+    }
+
+    type NameAge = Pick<Person, "name" | "age">
+
+    // Omit => Opposite of Pick
+    type ContactInfo = Omit<Person, "name" | "age">
+
+    // Required
+    type PersonRequired = Required<Person>
+
+    // Partial
+    type PersonPartial = Partial<Person>
+
+    // Readonly
+
+    type PersonReadOnly = Readonly<Person>
+
+    const person1: PersonReadOnly = {
+        name: "CHishtyy",
+        age: 200,
+        contactNo: "4234"
+    }
+
+    // Record
+    // type MyObj = {
+    //     a: string;
+    //     b: string;
+    // }
+
+    type MyObj = Record<string, string>
+
+    const emptyObj : Record<string, unknown> = {}
+
+    const obj1: MyObj = {
+        a: "aa",
+        b: "bb",
+        c: "cc"
+    }
 }
